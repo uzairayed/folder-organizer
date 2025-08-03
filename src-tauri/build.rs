a650cd3693d374f8130b30f27492dbb019e967da
uzairayed <uzairayed@gmail.com>
2025-08-03 18:12:43 +0500

    refactor: Clean up project to CLI-only version, remove unnecessary files

--- src-tauri/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // Skip tauri-build for now to avoid icon requirements
-    // tauri_build::build()
-    
-    // Just set some basic build flags
-    println!("cargo:rerun-if-changed=tauri.conf.json");
-    println!("cargo:rustc-cfg=desktop");
-} 
